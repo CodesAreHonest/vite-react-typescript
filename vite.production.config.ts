c41627b86d56c824cb8602719fc90e3ad027a364
Yinghua Chai <yinghuachai0@gmail.com>
2021-04-21 15:10:47 +0800

    chore: bundle setup for dev and prod

--- vite.production.config.ts
+++ vite.production.config.ts
@@ -5,27 +5,31 @@ const libEntryPath = path.resolve(__dirname, "src/main.tsx");
 const outputDir = path.resolve(__dirname, "dist");
 
 export default defineConfig({
-  mode: "development",
+  mode: "production",
   logLevel: "info",
   clearScreen: false,
   build: {
     outDir: outputDir,
-    sourcemap: true,
-    minify: false,
+    sourcemap: false,
+    minify: "terser",
     emptyOutDir: false,
     lib: {
       entry: libEntryPath,
       formats: ["es", "cjs", "umd"],
-      name: "motif",
+      name: "test",
+      fileName: "test",
     },
-    // rollupOptions: {
-    //   input: libEntryPath,
-    //   output: {
-    //     dir: outputDir,
-    //     name: "MyBundle",
-    //     format: "umd",
-    //   },
-    // },
-    brotliSize: false,
+    brotliSize: true,
+    chunkSizeWarningLimit: 1000,
+    terserOptions: {
+      ecma: 5,
+      ie8: false,
+      keep_classnames: false,
+      keep_fnames: false,
+      mangle: true,
+      compress: true,
+      sourceMap: false,
+      safari10: false,
+    },
   },
 });
